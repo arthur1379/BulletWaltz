4a757037ef5e7faef30a43e22bebe19619796ceb
arthur1379 <changhsinlin1206@gmail.com>
2017-03-12 22:53:03 +0800

    新增程式碼
    新增程式碼

---
+++ BulletWaltz/Assets/TurretManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretManager : MonoBehaviour {
+
+    private Animator _animator; // 宣告一個Animaotor型態的元件
+
+	void Start () {
+        _animator = this.GetComponent<Animator>(); // 指定Turret物件中的Animator元件進來
+	}
+
+    private void PlayShootAnimation(){ // 自行定義一個 PlayShootAnimation 函式
+        _animator.SetTrigger("Shoot"); // 觸發Animator狀態機中的 Trigger - Shoot
+    }
+
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)){ // 當偵測到空白鍵按下 執行 PlayShootAnimation 函式
+            PlayShootAnimation();
+        }
+	}
+}
