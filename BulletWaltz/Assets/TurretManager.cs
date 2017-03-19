b45ba80e76ef15085ffd9fbf829fe74f46d6a1c4
arthur1379 <changhsinlin1206@gmail.com>
2017-03-19 21:10:44 +0800

    ChangeScriptToPublic

--- BulletWaltz/Assets/TurretManager.cs
+++ BulletWaltz/Assets/TurretManager.cs
@@ -19,12 +19,12 @@ public class TurretManager : MonoBehaviour {
         _animator = this.GetComponent<Animator>(); // 指定Turret物件中的Animator元件進來
 	}
 
-    private void PlayShootAnimation(){ // 自行定義一個 PlayShootAnimation 函式
+    public void PlayShootAnimation(){ // 自行定義一個 PlayShootAnimation 函式
         _animator.SetTrigger("Shoot"); // 觸發Animator狀態機中的 Trigger - Shoot
         GameCamera.transform.DOShakePosition(CameraShakeDuration, CameraShakeStrenth); // 讓相機有震動的功能
     }
 
-    private void PlayRotateAnimation() // 控制Turret的旋轉
+    public void PlayRotateAnimation() // 控制Turret的旋轉
     {
         float targetDegree = 360.0f / DirectionCount * Random.Range(0, DirectionCount); // targetDegree 的數字使用隨機的數字來產生
         this.transform.DORotate(new Vector3(0, 0, targetDegree), rotateDuration); // 讓Turret的旋轉產生圓滑的效果
